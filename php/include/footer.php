29395d154f80f2f43474924f9b5219cdb8fc595e
Chirag s <chirag748317@gmail.com>
2024-10-12 13:17:18 +0530

    finally

---
+++ php/include/footer.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+$(document).ready(function () {
+  $('.btn-notice').click(function (e) {
+    $('.bg-notice').hide();
+    $(this).hide();
+  });
+});
+</script>
+<style type="text/css">
+  @media only screen and (min-width: 387px) {
+    .btn-notice {bottom:36px;}  
+  }
+  @media only screen and (max-width: 386px) {
+    .btn-notice {bottom: 52px;}
+  }
+</style>
---
+++ php/info.php
@@ -0,0 +1,8 @@
+<?php 
+$base_url = "//{$_SERVER['SERVER_NAME']}";
+$website_name = "animeverse";
+$apiLink = "https://anime-api-uv3s.onrender.com"; 
+if (date("d") > 15){
+    $apiLink = "https://anime-api-uv3s.onrender.com";
+}
+?>
